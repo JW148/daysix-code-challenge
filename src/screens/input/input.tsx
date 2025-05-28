9af780f3293e8a6d388eb496c31a19cf2b741f3a
jw148 <wiggin472@gmail.com>
2025-05-28 10:34:44 +0100

    Implement Input component / page
    - layout
    - styling
    - sit stand gif
    - concentric circles remain perfect circles across different viewport sizes
    - end test button

--- src/screens/input/input.tsx
+++ src/screens/input/input.tsx
@@ -1,10 +1,27 @@
 import "./input.css";
+import gif from "../../assets/sit_stand.gif";
+import { Pencil } from "lucide-react";
 
 export const Input = () => {
   return (
-    <div>
-      <h1>Input</h1>
-      <p>This is the input screen.</p>
+    <div className="inputContainer">
+      <div className="inputBody">
+        <h1>Sit & Stand Test</h1>
+        <div className="gifContainer">
+          <img src={gif} alt="Sit stand gif" className="gif" />
+          <h1 className="timer">00:00</h1>
+        </div>
+        <div className="footerContainer">
+          <div className="repsCounter">
+            <h1 className="repsText">Reps Counter</h1>
+            <h1>10</h1>
+            <Pencil color="white" />
+          </div>
+          <div className="endTestBtn">
+            <p>End Test</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
